19901435a4ebae018b621c6263434f81c41735c4
osequi <cs@osequi.com>
2020-10-06 18:42:32 +0300

    Removing the old Layout

--- src/components/layout/Layout/Layout.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Layout from "./Layout";
-
-it("has a Layout component", () => {
-  const { getByText } = render(<Layout />);
-  expect(getByText("Layout")).toBeInTheDocument();
-});
--- src/components/layout/Layout/index.js
+++
@@ -1,2 +0,0 @@
-export { default, LayoutPropTypes, LayoutDefaultProps } from "./Layout";
-export { default as LayoutDemo } from "./Layout.demo";
