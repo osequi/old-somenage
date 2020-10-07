ecc5bf3abbec489a60974f473aa8e354a690ab73
osequi <cs@osequi.com>
2020-10-07 10:57:02 +0300

    Working on the cube

--- src/components/layout/cube/Cube/Cube.demo.js
+++ src/components/layout/cube/Cube/Cube.demo.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import clsx from "clsx";
 
 import Cube from ".";
 import { Section, Article } from "../../../semantic-elements/SemanticElements";
 
 /**
+ * Defines the styles
+ */
+const useStyles = makeStyles((theme) => ({
+  default: {
+    background: "lightblue",
+  },
+}));
+
+/**
  * Displays the component
  */
 const CubeDemo = (props) => {
+  const { default: defaultKlass } = useStyles(props);
+
   return (
     <Section title="CubeDemo">
-      <Article title="Default">
+      <Article title="Default" className={defaultKlass}>
         <Cube />
       </Article>
     </Section>
